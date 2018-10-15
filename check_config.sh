4011c832f5de57c14774c98efa74f95c5b071f35
Tom Hoover <tom@hisword.net>
2018-10-15 10:54:30 -0500

    verify on config is on master branch before HA restart

--- check_config.sh
+++ check_config.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 docker exec -it home-assistant python -m homeassistant --config /config --script check_config
+BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+RED=`tput setaf 1`
+RESET=`tput sgr0`
 echo ""
-read -n 1 -s -r -p "Restart Home Assistant? [Y/n] " YN
-[[ $YN == "y" || $YN == "Y" || $YN == "" ]] && echo "" && echo "  Restarting..." && docker restart home-assistant
-echo ""
+if [ "$BRANCH" != "master" ] ; then
+    read -n 1 -s -r -p "${RED}YOU ARE NOT ON THE MASTER BRANCH.${RESET} Are you sure you wish to restart Home Assistant? [Y/n] " YN
+else
+    read -n 1 -s -r -p "Restart Home Assistant? [Y/n] " YN
+fi
+    [[ $YN == "y" || $YN == "Y" || $YN == "" ]] && echo "" && echo "  Restarting..." && docker restart home-assistant && ssh pizw-0 sudo reboot
+    echo ""
